c1939ae50ffff8ec0abf58fe4a6b2dced84b0de4
Vivek Kumar Singh <100799747+krviveksingh@users.noreply.github.com>
2022-03-03 02:17:22 +0530

    Add files via upload

---
+++ largest_and_smallest_elements_of_array.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number of elements"<<endl;
+    cin>>n;
+    int a[n];
+    cout<<"Enter the elements of the array"<<endl;
+    for (int i=1;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int largest = a[1];
+    for(int i=1;i<n;i++)
+    {
+        if(a[i]>largest)
+        {
+            largest = a[i];
+        } 
+    }
+    int smallest = a[1];
+    for(int i=1;i<n;i++)
+    {
+        if(a[i]<smallest)
+        {
+            smallest = a[i];
+        } 
+    }
+    
+    cout<<"The largest value is"<<" "<<largest<<endl;
+    cout<<"The smallest value is"<<" "<<smallest<<endl;
+    return 0;
+}
